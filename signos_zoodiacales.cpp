e84a10ac051ad8894245b86a41925057181873c0
Wilmer Hernández-Álvarez <wilmer.hernandez@gmail.com>
2021-05-24 11:52:07 -0500

    Update signos_zoodiacales.cpp

--- signos_zoodiacales.cpp
+++ signos_zoodiacales.cpp
@@ -29,12 +29,35 @@ main (){
 	//Puede usar más estructuras de control si es necesario.
 	
 	// Capricornio ---> Diciembre 22 – Enero 19
-	if (mesNacimiento == 1 ||  mesNacimiento == 12)//Enero y diciembre
+	if (mesNacimiento == 12 || mesNacimiento == 1)//Diciembre y enero
 	{
-		if( (diaNacimiento >= 22 && mesNacimiento == 12) || (diaNacimiento <= 19 && mesNacimiento == 1))
+		if((diaNacimiento >= 22 && mesNacimiento == 12) || (diaNacimiento <= 19 && mesNacimiento == 1))
 		{
 			cout << "Su signo es Capricornio";
 		}
 	}
 	
+	//Sagitario ------> Noviembre 22 - Diciembre 21
+	if (mesNacimiento == 11 || mesNacimiento == 12)//Noviembre y diciembre
+	{
+		if((diaNacimiento >= 22 && mesNacimiento == 11) || (diaNacimiento <= 21 && mesNacimiento == 12))
+		{
+			cout << "Su signo es Sagitario";
+		}
+	}
+	
+	//Escorpio ------> Octubre 23 – Noviembre 21
+	if (mesNacimiento == 10 || mesNacimiento == 11)//Octubre y noviembre
+	{
+		if((diaNacimiento >= 23 && mesNacimiento == 10) || (diaNacimiento <= 21 && mesNacimiento == 11))
+		{
+			cout << "Su signo es Escorpio";
+		}
+	}
 }
+
+
+
+
+
+
