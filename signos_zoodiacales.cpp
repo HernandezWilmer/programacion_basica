79ef2ad8e3aa1a769a54471237140dc5b923b7c9
Wilmer Hernández-Álvarez <wilmer.hernandez@gmail.com>
2021-05-24 12:05:52 -0500

    Update signos_zoodiacales.cpp

--- signos_zoodiacales.cpp
+++ signos_zoodiacales.cpp
@@ -1,39 +1,41 @@
 /*El estudiante debe desarrollar un programa que que calcule y muestre el signo zodiacal de una persona 
-a partir de su fecha de su nacimiento.
-
-El orden lógico sugerido es:
-
-1. Pídale al usuario que digite el día (número entero) de nacimiento.
-2. Pídale al usuario que digite el mes (número entero) de nacimiento.
-3. Utilice if anidados para calcular el signo zodiacal del usuario. Puede usar más estructuras de control 
-si es necesario.
-4. Muestre un mensaje parecido a este: Carolina nació el 3 de enero, por lo tanto, su signo zodiacal es 
-capricornio.
-Información adicional: https://www.astrology-zodiac-signs.com/es/*/
+a partir de su fecha de su nacimiento.*/
 
 #include <iostream>
 using namespace std;
 
 int diaNacimiento, mesNacimiento;
+string signo, nombre, mesTexto;
 
 main (){
-	//1. Pídale al usuario que digite el día (número entero) de nacimiento.
+	cout << "Digite su nombre: ";
+	cin >> nombre;
+	
 	cout << "Digite el dia de su nacimiento (numero entero): ";
 	cin >> diaNacimiento;
 	
-	//2. Pídale al usuario que digite el mes (número entero) de nacimiento.
 	cout << "Digite el mes de su nacimiento (numero entero): ";
 	cin >> mesNacimiento;
 	
-	//3. Utilice if anidados para calcular el signo zodiacal del usuario.
-	//Puede usar más estructuras de control si es necesario.
+	switch(mesNacimiento)
+	{
+		case 1:
+			mesTexto = "enero";
+			break;
+		case 2:
+			mesTexto = "febrero";
+			break;
+		case 3:
+			mesTexto = "marzo";
+			break;
+	}
 	
 	// Capricornio ---> Diciembre 22 – Enero 19
 	if (mesNacimiento == 12 || mesNacimiento == 1)//Diciembre y enero
 	{
 		if((diaNacimiento >= 22 && mesNacimiento == 12) || (diaNacimiento <= 19 && mesNacimiento == 1))
 		{
-			cout << "Su signo es Capricornio";
+			signo = "Capricornio";
 		}
 	}
 	
@@ -42,7 +44,7 @@ main (){
 	{
 		if((diaNacimiento >= 22 && mesNacimiento == 11) || (diaNacimiento <= 21 && mesNacimiento == 12))
 		{
-			cout << "Su signo es Sagitario";
+			signo = "Sagitario";
 		}
 	}
 	
@@ -51,13 +53,28 @@ main (){
 	{
 		if((diaNacimiento >= 23 && mesNacimiento == 10) || (diaNacimiento <= 21 && mesNacimiento == 11))
 		{
-			cout << "Su signo es Escorpio";
+			signo = "Escorpio";
 		}
 	}
+	
+	//Libra ---> Septiembre 23 – Octubre 22
+	if (mesNacimiento == 9 || mesNacimiento == 10)//Septiembre y octubre 
+	{
+		if((diaNacimiento >= 23 && mesNacimiento == 9) || (diaNacimiento <= 22 && mesNacimiento == 10))
+		{
+			signo = "Libra";
+		}
+	}
+	
+	// Piscis ---> Febrero 19 – Marzo 20
+	if (mesNacimiento == 2 || mesNacimiento == 3)//Febrero y marzo
+	{
+		if((diaNacimiento >= 2 && mesNacimiento == 2) || (diaNacimiento <= 20 && mesNacimiento == 10))
+		{
+			signo = "Piscis";
+		}
+	}
+
+	//Carolina nació el 3 de enero, por lo tanto, su signo zodiacal es capricornio.
+	cout << nombre << " nacio el " << diaNacimiento << " de " << mesTexto << ", por lo tanto, su signo zoodiacal es " << signo;	
 }
-
-
-
-
-
-
